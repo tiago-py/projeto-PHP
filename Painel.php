014fc70b9714326f31ac400ac2cfdadf6d5f0908
tiago-py <tiago.braga0012@gmail.com>
2023-08-29 14:42:16 -0300

    firts commit

--- Painel.php
+++ Painel.php
@@ -2,4 +2,7 @@
 require_once("session.php");
 echo "<h2>". $_SESSION['username']. " </h2>";
 echo "Logado com Sucesso";
+//fazer as alterações da página principal e criar mais 4
+//tenta colocar um header e nesse header 4 link sendo eles "sobre" "contato" "datas especiais" "sair" "curiosidades" ou coisas parecidas
+//tentar colocar um footer com o nome dos integrantes do grupo
 ?>
